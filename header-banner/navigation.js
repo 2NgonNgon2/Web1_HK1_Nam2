56ba7b3991686ddf640ef84127d252481d01b600
Nguyen Hoang Luan <cfdluan@gmail.com>
2022-10-29 22:01:53 +0700

    form dang ki

--- header-banner/navigation.js
+++ header-banner/navigation.js
@@ -1,30 +1,50 @@
-var signinMenu = document.getElementById("signin");
-var signupMenu = document.getElementById("signup");
-
-
-
+// scoll header menu bar
 window.addEventListener('scroll',function () {
     var header = document.querySelector('header');
     header.classList.toggle("sticky",window.scrollY > 0);
 });
+// ham display ra menu dang nhap
 function displaySignMenu(input){
+    var loginMenu = document.getElementById("login");
+    var createMenu = document.getElementById("create");
     if(input=="Sign in")
     {
-        signinMenu.style.display="block";
+        loginMenu.style.display="block";
+        createMenu.style.display="none";
     }
-    else
+    else if(input=="Sign up")
     {
-        signupMenu.style.display="block";
+        createMenu.style.display="block";
+        loginMenu.style.display="none";
+    }
+    else{
+        loginMenu.style.display="none";
+        createMenu.style.display="none";
     }
 }
-function disableMenu(input){
-    if(input=="Sign in")
-    {
-        signinMenu.style.display="none";
+function createAcc() {
+	var username = document.getElementById("form-Name").value;
+	var password = document.getElementById("form-Password").value;
+	var phone = document.getElementById("form-Phone").value;
+	var email = document.getElementById("form-E-mail").value;
+    var user = {
+        username : username,
+        password : password,
+        phone : phone,
+        email : email,
+    } 
+	var json = JSON.stringify(user);
+	localStorage.setItem(username,json);
+}
+function dangnhap(){
+    var username = document.getElementById("name").value;
+	var password = document.getElementById("pass").value;
+    var userLocal=JSON.parse(localStorage.getItem(username));
+    if(username==userLocal.username&&password==userLocal.password){
+        alert("dntk")
     }
-    else
-    {
-        signupMenu.style.display="none";
+    else{
+        alert("njnki");
     }
 }
 
