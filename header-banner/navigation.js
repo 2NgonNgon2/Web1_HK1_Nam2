d1fea9e473fe5be6c313223bf5e831fef9ddfa25
hnoga-n <hoanghuynguyen711@gmail.com>
2022-10-31 20:46:25 +0700

    added account.js and updated menu-pro navigation.js fils

--- header-banner/navigation.js
+++ header-banner/navigation.js
@@ -1,36 +1,3 @@
-
-let account = [
-    {
-        id:"1",
-        username:"admin",
-        password:"123",
-        email:"admin123@gmail.com",
-        phone:"123456"
-    },
-    {
-        id:"2",
-        username:"admin2",
-        password:"123",
-        email:"admin123@gmail.com",
-        phone:"123456"
-    }
-    ,
-    {
-        id:"3",
-        username:"admin3",
-        password:"123",
-        email:"admin123@gmail.com",
-        phone:"123456"
-    }
-    ,
-    {
-        id:"4",
-        username:"admin4",
-        password:"123",
-        email:"admin123@gmail.com",
-        phone:"123456"
-    }
-];
 $(document).ready(function(){
     account.forEach(element => {
         var json = JSON.stringify(element);
@@ -65,8 +32,11 @@ function dangnhap(){
     var username = document.getElementById("name").value;
 	var password = document.getElementById("pass").value;
     var userLocal=JSON.parse(localStorage.getItem(username));
-    if(username==userLocal.username&&password==userLocal.password){
-        alert("Dang nhap thanh cong")
+
+    if(username==userLocal.username && password==userLocal.password && userLocal.authority =="admin"){
+        alert("Dang nhap thanh cong");
+        isSignedin = true;
+        console.log("you are admin");
     }
     else{
         alert("Error!");
