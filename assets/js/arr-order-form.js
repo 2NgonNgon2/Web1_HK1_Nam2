a7414e5d474b04573d42798f1b2a91972f602f43
hnoga-n <hoanghuynguyen711@gmail.com>
2022-11-21 21:07:46 +0700

    .

--- assets/js/arr-order-form.js
+++ assets/js/arr-order-form.js
@@ -1,79 +1,43 @@
-// let orderForm = [];//mảng đơn hàng
+let orderForm = [];//mảng đơn hàng
 // let orderFormItem ={};//thêm đơn hàng 
 let lenghtOrderForm = 1;
-
+localStorage.removeItem("orderForm");
 if(localStorage.getItem("orderForm") == null) // orderForm chưa có trong localStoragge 
 {
     console.log("yo wtf?");
-    orderForm = [ 
+    orderForm = [
         {
-            id: "1",
-            idUser: "3",
-            dateOrder: "02/11/2020",
-            status: true,
-            arrProductId: ['5','24','11','20','2','2'],
-            totalPrice: "1999999"
-        },
-        {
-            id: "2",
-            idUser: "3",
-            dateOrder: "02/02/2022",
-            status: true,
-            arrProductId: ['14','6','2','10'],
-            totalPrice: "1999999"
-        },
-        {
-            id: "3",
-            idUser: "4",
-            dateOrder: "02/11/2021",
-            status: false,
-            arrProductId: ["14","6"],
-            totalPrice: "1999999"
-        },
-        {
-            id: "4",
-            idUser: "4",
-            dateOrder: "02/05/2020",
-            status: false,
-            arrProductId: ["4","6","21"],
-            totalPrice: "1999999"
-        },
-        {
-            id: "5",
-            idUser: "4",
-            dateOrder: "02/07/2021",
-            status: false,
-            arrProductId: ["1","26","22","10","15"],
-            totalPrice: "1999999"
-        },
-        {
-            id: "6",
-            idUser: "4",
-            dateOrder: "02/06/2022",
-            status: false,
-            arrProductId: ["1","26","22","10","15"],
-            totalPrice: "1999999"
-        },
-        {
-            id: "7",
-            idUser: "5",
-            dateOrder: "07/19/2022",
-            status: false,
-            arrProductId: ["1","26","22","10","15"],
-            totalPrice: "1999999"
-        }
-    
-    ]
+             id: "1",
+             idUser: "3",
+             dateOrder: today,
+             status: isProcessed,
+             arrProductId: ["4","24","11","20"]
+         },
+         {
+             id: "2",
+             idUser: "3",
+             dateOrder: today,
+             status: isProcessed,
+             arrProductId: ["14","6","2","10"]
+         },
+         {
+             id: "3",
+             idUser: "4",
+             dateOrder: today,
+             status: isProcessed,
+             arrProductId: ["14","6"]
+         },
+         {
+             id: "4",
+             idUser: "4",
+             dateOrder: today,
+             status: isProcessed,
+             arrProductId: ["4","6","21"]
+         },
+    ];
     localStorage.setItem("orderForm",JSON.stringify(orderForm));
 
 }
-if(localStorage.getItem("orderForm") != null)  // đã có trong ls(dùng trong trường hợp chỉnh sửa)
-{
-    orderForm = JSON.parse(localStorage.getItem("orderForm"));
-
-}
-
-
 
 function getDate() {
     let today = new Date();
