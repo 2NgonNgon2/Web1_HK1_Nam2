f6747abfcd8e684b78a4952143ca6c069cd76f88
Nguyen Hoang Luan <cfdluan@gmail.com>
2022-11-23 15:56:49 +0700

    vẫn là form đăng nhập

--- assets/js/arr-account.js
+++ assets/js/arr-account.js
@@ -1,37 +1,44 @@
 // mảng tài khoản
 
-var account = 
-[
-    {
-        "id": "1",
-        "username": "admin",
-        "password": "123",
-        "authority": "admin",
-        "email": "admin123@gmail.com",
-        "phone": "123456"
-    },
-    {
-        "id": "2",
-        "username": "admin2",
-        "password": "123",
-        "authority": "admin",
-        "email": "admin123@gmail.com",
-        "phone": "123456"
-    },
-    {
-        "id": "3",
-        "username": "user",
-        "password": "123",
-        "authority": "user",
-        "email": "admin123@gmail.com",
-        "phone": "123456"
-    },
-    {
-        "id": "4",
-        "username": "user2",
-        "password": "123",
-        "authority": "user",
-        "email": "admin123@gmail.com",
-        "phone": "123456"
-    }
-];
+var account = [];
+if (localStorage.getItem("arr-account") != null) {
+    account = JSON.parse(localStorage.getItem("arr-account"));
+}
+else if(localStorage.getItem("arr-account" ) === null) {
+    account =
+        [
+            {
+                "id": "1",
+                "username": "admin",
+                "password": "123",
+                "authority": "admin",
+                "email": "admin123@gmail.com",
+                "phone": "123456"
+            },
+            {
+                "id": "2",
+                "username": "admin2",
+                "password": "123",
+                "authority": "admin",
+                "email": "admin123@gmail.com",
+                "phone": "123456"
+            },
+            {
+                "id": "3",
+                "username": "user",
+                "password": "123",
+                "authority": "user",
+                "email": "admin123@gmail.com",
+                "phone": "123456"
+            },
+            {
+                "id": "4",
+                "username": "user2",
+                "password": "123",
+                "authority": "user",
+                "email": "admin123@gmail.com",
+                "phone": "123456"
+            }
+        ];
+    localStorage.setItem("arr-account", JSON.stringify(account));
+}
