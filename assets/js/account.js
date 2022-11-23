6b56c8b8dbbbac6abf28104701cf3c503825182c
hnoga-n <hoanghuynguyen711@gmail.com>
2022-11-23 13:42:28 +0700

    .

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,24 +1,33 @@
 let isAdmin;
 let isSignedin = false;
 
-if(localStorage.getItem("isSignedin") != "true")
-{
+$(document).ready(function () {
+    //localStorage.clear;
+
+    account.forEach(element => {
+        localStorage.setItem(element.username, JSON.stringify(element));
+    });
+
+    if (location.href == '/admin.html') {
+        document.querySelector("span.dropdown-select").innerHTML = "Admin";
+    }
+});
+
+if (localStorage.getItem("isSignedin") != "true") {
     isSignedin = false;
 }
-else
-{
+else {
     isSignedin = true;
 }
 
-if(localStorage.getItem("adminSignedin") != null)
-{
-    window.location.href = "/admin.html";
+if (localStorage.getItem("adminSignedin") != null) {
+    document.querySelector("span.dropdown-select").innerHTML = localStorage.getItem("adminSignedin");
 }
 
-if(localStorage.getItem("userSignIn") != null){
-    document.querySelector("span.dropdown-select").innerHTML=localStorage.getItem("userSignIn");  
+if (localStorage.getItem("userSignIn") != null) {
+    document.querySelector("span.dropdown-select").innerHTML = localStorage.getItem("userSignIn");
     var dropdown_list = document.querySelector(".dropdown .dropdown-list");
-        dropdown_list.innerHTML=`
+    dropdown_list.innerHTML = `
             <li class="dropdown-item">
                 <span class="dropdown-text">Thông tin cá nhân</span>
             </li>
@@ -26,83 +35,81 @@ if(localStorage.getItem("userSignIn") != null){
                 <span class="dropdown-text">Xem đơn hàng đã đặt</span>
             </li>
             <li class="dropdown-item">
-                <span class="dropdown-text" onclick="dangXuat()">Đăng xuất</span>
+                <span class="dropdown-text">Đăng xuất</span>
             </li>
                 `;
-    localStorage.setItem("isSignedin","true");  
-
-} 
-
-$(document).ready(function(){
-    account.forEach(element => {
-        //localStorage.clear;
-        var json = JSON.stringify(element);
-	    localStorage.setItem(element.username,json);
-    });
-    if(location.href=='/admin.html'){
-        document.querySelector("span.dropdown-select").innerHTML="Admin"; 
-    }
-});
-function dangnhap(event){
-    var username = document.getElementById("name").value;
-    var password = document.getElementById("pass").value;
-    var userLocal=JSON.parse(localStorage.getItem(username));
-    if(username==userLocal.username && password==userLocal.password && userLocal.authority =="admin"){
-        localStorage.setItem("adminSignedin",username);
-        localStorage.setItem("isSignedin","true"); 
-        window.location.href = "/admin.html";
-        event.preventDefault(); // ngăn form không bị reload sau khi submit
-    }
-    else if(username==userLocal.username && password==userLocal.password && userLocal.authority =="user"){
-        localStorage.setItem("isSignedin","true");
-        localStorage.setItem("userSignIn",username);
-        event.preventDefault();
-        window.location.reload();
-        console.log("you are user");   
-    }
-    else{
-        alert("Error!");
-    }
-
-      $(".dropdown-item:contains('Đăng xuất')").click(function(){
-        if(isSignedin==true){
-            isSignedin= false;
-            dropdown_list.innerHTML=`
+    localStorage.setItem("isSignedin", "true");
+    $(".dropdown-item:contains('Đăng xuất')").click(function () {
+        if (isSignedin == true) {
+            isSignedin = false;
+            dropdown_list.innerHTML = `
                 <li class="dropdown-item" onclick="displaySignMenu('Sign in')" >
                     <span class="dropdown-text" id="sign-in">Đăng nhập</span>
                 </li>
                 <li class="dropdown-item" onclick="displaySignMenu('Sign up')" >
                     <span class="dropdown-text" id="sign-up">Đăng kí</span>
                 </li>
-          `; 
-        }  
-        document.querySelector("span.dropdown-select").innerHTML="My account"; 
+          `;
+        }
+        document.querySelector("span.dropdown-select").innerHTML = "My Account";
         localStorage.removeItem("userSignIn");
-    }); 
-    backgroundLogin.style.display="none";
+        localStorage.setItem("isSignedin", "false");
+        window.location.reload();
+    });
 }
 
-function dangXuat()
-{
-    dropdown_list.innerHTML=`
-                <li class="dropdown-item" onclick="displaySignMenu('Sign in')" >
-                    <span class="dropdown-text" id="sign-in">Đăng nhập</span>
-                </li>
-                <li class="dropdown-item" onclick="displaySignMenu('Sign up')" >
-                    <span class="dropdown-text" id="sign-up">Đăng kí</span>
-                </li>
-          `; 
+function dangXuatAdmin() {
+    localStorage.setItem("isSignedin", "false");
+    localStorage.removeItem("adminSignedin");
+    window.location.href = "/index.html";
+}
 
-    document.querySelector("span.dropdown-select").innerHTML="My account"; 
-    localStorage.removeItem("userSignIn");
-    localStorage.setItem("isSignedin","false");   
+function dangnhap(event) {
+    try {
+        var username = document.getElementById("name").value;
+        var password = document.getElementById("pass").value;
+        var userLocal = JSON.parse(localStorage.getItem(username));
+        if (username == userLocal.username && password == userLocal.password && userLocal.authority == "admin") {
+            localStorage.setItem("adminSignedin", username);
+            localStorage.setItem("isSignedin", "true");
+            window.location.href = "/admin.html";
+            event.preventDefault(); // ngăn form không bị reload sau khi submit
+        }
+        else if (username == userLocal.username && password == userLocal.password && userLocal.authority == "user") {
+            localStorage.setItem("isSignedin", "true");
+            localStorage.setItem("userSignIn", username);
+            event.preventDefault();
+            window.location.reload();
+            console.log("you are user");
+        }
+        else{
+            alert("Error!");
+        }
+        backgroundLogin.style.display = "none";
+    } catch (err){
+        alert(err);
+    }
+
+}
+
+function createAcc(event) {
+    var authority = "user";
+    var username = document.getElementById("form-Name").value;
+    var password = document.getElementById("form-Password").value;
+    var phone = document.getElementById("form-Phone").value;
+    var email = document.getElementById("form-Email").value;
+    var user = {
+        username: username,
+        password: password,
+        phone: phone,
+        email: email,
+        authority: authority,
+    }
+    localStorage.setItem(username, JSON.stringify(user));
+    alert("Dang ki thanh cong");
+    event.preventDefault();
     window.location.reload();
-
-};
-
-
-
-
+}
 
 
 
