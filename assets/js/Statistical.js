6ede5c8ea04fc47ecd52dcb4a3aa013393f62ac4
Nguyen Hoang Luan <cfdluan@gmail.com>
2022-12-01 20:27:51 +0700

    thống kê

--- assets/js/Statistical.js
+++ assets/js/Statistical.js
@@ -1,53 +1,60 @@
 
 var tmpOrderForm = JSON.parse(localStorage.getItem("orderForm")); // mảng hóa đơn
-var tmp = []; // danh sách các sản phẩm và số lượng từng loại
-var statisticalTable; // bang thong ke
 
-function Statistical(){
-    let i = get("11");
-    let total = 0 ; 
+
+
+function Statistical(input) {
+    var statisticalTable; // bang thong ke
+    let i = get(input); // lay ds da loc
+    console.log(i);
+    let total = 0;
     i.forEach(element => {
-        total+=parseInt(element.price)*element.quantity;
+        total += parseInt(element.price) * element.quantity;
     });
     statisticalTable = {
-        name : "Bảng thống kê tháng " + "" ,
-        total:total,
-        list : i
+        name: "Bảng thống kê tháng " + input,
+        total: total,
+        list: i
     }
     console.log(statisticalTable);
+    return statisticalTable;
+
 }
+function checkSl(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i].id === arr[j].id) {
+                arr[i].quantity++;
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
+}
+
 function get(date) {
-    let items = FitlerByMonth(date);// mảng hóa đơn sau khi lọc
+    var tmpItems = []; // danh sách các sản phẩm và số lượng từng loại tương ứng
+    let items = FitlerByMonth(date);// danh sách hóa đơn sau khi lọc
     items.forEach(element => {
-        let arr = element.arrProductId; // mang sản phẩm
+        let arrProduct = element.arrProductId; // danh sách sản phẩm
         let item;
-        arr.forEach(i => {
+        arrProduct.forEach(i => {
             item = {
+                type: i.type,
                 productName: i.name,
+                price: i.price,
                 quantity: i.quantity,
-                price : i.price
+                id: i.id
             }
-            if (tmp.length == 0) {
-                tmp.push(item);
-            }
-            else {
-                tmp.forEach(j => {
-                    if (j.productName == i.name) {
-                        j.quantity += i.quantity;
-                    }
-                    else {
-                        tmp.push(item);
-                    }
-                });
-            }
+            tmpItems.push(item);
         });
     });
-    return tmp;
+    return checkSl(tmpItems);
 }
 function FitlerByMonth(date) {
     let tmpArr = [];
     tmpOrderForm.forEach(element => {
-        if (Find(element.dateOrder) == date && !element.status) {
+        if (Find(element.dateOrder) == date && element.status) {
             tmpArr.push(element);
         }
     });
@@ -59,10 +66,114 @@ function Find(str) {
     let lastindex = str.lastIndexOf("/");
     return str.slice(index + 1, lastindex);
 }
-$(".btn").click(function () {
-    console.log(tmpOrderForm);
-    console.log(Find("12/11/2002"));
-    console.log(FitlerByMonth("11"));
-    console.log(get("11"));
-    Statistical();
+
+/* HIỂN THỊ BẢNG THỐNG KÊ  (Statistical)*/
+$(".nav-header-left-list-business-statistics").click(function () {
+    let today = new Date();
+    containerContentAdmin.innerHTML = `
+    <div class="title">BẢNG THỐNG KÊ ${today.getFullYear()}</div>
+    <form class="date-filter" onsubmit="filterStatistical(event)">
+      <div class="container-nav-header-right-filter-type">
+      <label for="typeProducts">Chọn loại:</label>
+      <select name="typeProducts" id="typeProducts">
+          <option value="all">Tất cả</option>
+          <option value="keyboard">Bàn phím</option>
+          <option value="mouse">Chuột</option>
+          <option value="headphone">Tai nghe</option>
+          <option value="speaker">Loa</option>
+          <option value="Laptop">Laptop</option>
+      </select>
+      <label for="month">Lọc tháng:</label>
+      <select id='gMonth'>
+          <option value=''>--Select Month--</option>
+          <option selected value='1'>Janaury</option>
+          <option value='2'>February</option>
+          <option value='3'>March</option>
+          <option value='4'>April</option>
+          <option value='5'>May</option>
+          <option value='6'>June</option>
+          <option value='7'>July</option>
+          <option value='8'>August</option>
+          <option value='9'>September</option>
+          <option value='10'>October</option>
+          <option value='11'>November</option>
+          <option value='12'>December</option>
+      </select> 
+      </div> 
+      <button type="submit" class="filter">LỌC</button>
+    </form>
+    <div class="Statistical">
+    
+    </div>
+    `
 });
+
+function filterStatistical(event) {
+    event.preventDefault();
+
+    let typeProducts = document.querySelector("#typeProducts").value;
+    let month = document.getElementById("gMonth").value;
+    let filterArr = [];
+
+    let table = Statistical(month);
+
+    if (typeProducts === "all") {
+        renderStatistical(table, month);
+    }
+    else {
+        table.list.forEach(element => {
+            if (element.type === typeProducts) {
+                filterArr.push(element);
+            }
+        });
+        let table1999 = table;
+        table1999.list = filterArr;
+
+        renderStatistical(table1999, month);
+    }
+
+
+}
+
+function renderStatistical(table, month) {
+
+    const StatisticalDisplay = document.querySelector(".Statistical");
+    let StatisticalItem = "";
+    StatisticalItem =
+        `
+  <div class="header">
+  <div class="STT">STT</div>
+  <div class="Statistical-PRODUCT-TYPE">LOẠI</div>
+  <div class="Statistical-PRODUCT-NAME">SẢN PHẨM</div>
+  <div class="Statistical-PRICE">GIÁ</div>
+  <div class="QUALITY">SỐ LƯỢNG</div>
+  <div class="TOTAL">TỔNG</div>
+  </div>
+  `
+    let i = 1;
+    let list = table.list;
+    list.forEach(element => {
+        StatisticalItem += `
+    <div class="Statistical-item">
+        <div class="STT">${i++}</div>
+        <div class="Statistical-PRODUCT-TYPE">${element.type}</div>
+        <div class="Statistical-PRODUCT-NAME">${element.productName}</div>
+        <div class="Statistical-PRICE">${format2(parseInt(element.price), 'đ')}</div>
+        <div class="QUALITY">${element.quantity}</div>
+        <div class="TOTAL">${format2(parseInt(element.quantity * element.price), 'đ')}</div>
+    </div>
+    `
+    });
+    StatisticalItem += `
+    <div class="footer">
+        <div class="line">Tổng doanh thu trong tháng ${month}</div>
+        <div class="Total-revenue">${format2(parseInt(table.total), 'đ')}</div>
+    </div>
+    `
+
+    StatisticalDisplay.innerHTML = StatisticalItem;
+}
+
+function format2(n, currency) {
+    return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + currency;
+}
